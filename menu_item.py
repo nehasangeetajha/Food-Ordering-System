1f7f848f7562d0b529075e582f235c2f2e948c56
Sangeeta Jha <43818333+nehasangeetajha@users.noreply.github.com>
2020-02-09 16:52:26 +0530

    Update menu_item.py

--- menu_item.py
+++ menu_item.py
@@ -8,11 +8,8 @@ class MenuItem:
 
     def get_total_price(self, count):
         total_price = self.price * count
-        
-        # If count is 3 or higher, multiply it by 0.9
-        if count>=3 :
-            total_price*=0.9
+
+        if count >= 3:
+            total_price *= 0.9
+
         return round(total_price)
-        
-        # Round total_price to the nearest whole number and return it
-        
